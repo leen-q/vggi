96e83156f4ad7653ef844e9a64fbdefb08d6b9cd
Leenqq <eugeneverbitskiy84@gmail.com>
2023-12-09 22:07:06 +0200

    Add lightning and Gouraud shading

--- main.js
+++ main.js
@@ -14,12 +14,16 @@ function deg2rad(angle) {
 function Model(name) {
     this.name = name;
     this.iVertexBuffer = gl.createBuffer();
+    this.iNormalBuffer = gl.createBuffer();
     this.count = 0;
 
-    this.BufferData = function(vertices) {
+    this.BufferData = function(vertices, normal) {
 
         gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iNormalBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normal), gl.STREAM_DRAW);
 
         this.count = vertices.length/3;
     }
@@ -29,11 +33,14 @@ function Model(name) {
         gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
         gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(shProgram.iAttribVertex);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iNormalBuffer);
+        gl.vertexAttribPointer(shProgram.iAttribNormal, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribNormal);
    
-        gl.drawArrays(gl.LINE_STRIP, 0, this.count);
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, this.count);
     }
 }
-
 
 // Constructor
 function ShaderProgram(name, program) {
@@ -43,10 +50,15 @@ function ShaderProgram(name, program) {
 
     // Location of the attribute variable in the shader program.
     this.iAttribVertex = -1;
-    // Location of the uniform specifying a color for the primitive.
-    this.iColor = -1;
+
+    this.iAttribNormal = -1;
+
+    this.iLightPosition = -1;
+
     // Location of the uniform matrix representing the combined transformation.
     this.iModelViewProjectionMatrix = -1;
+
+    this.iModelMatrixNormal = -1;
 
     this.Use = function() {
         gl.useProgram(this.prog);
@@ -63,7 +75,7 @@ function draw() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     
     /* Set the values of the projection transformation */
-    let projection = m4.perspective(Math.PI/8, 1, 8, 12); 
+    let projection = m4.orthographic(-2, 2, -2, 2, 8, 12);
     
     /* Get the view matrix from the SimpleRotator object.*/
     let modelView = spaceball.getViewMatrix();
@@ -78,37 +90,107 @@ function draw() {
        combined transformation matrix, and send that to the shader program. */
     let modelViewProjection = m4.multiply(projection, matAccum1 );
 
+    let inversion = m4.inverse(modelViewProjection);
+    let transposedModel = m4.transpose(inversion);
+
     gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection );
-    
-    /* Draw the six faces of a cube, with different colors. */
-    gl.uniform4fv(shProgram.iColor, [1,1,0,1] );
+    gl.uniformMatrix4fv(shProgram.iModelMatrixNormal, false, transposedModel );
+    gl.uniform3fv(shProgram.iLightPosition, [0.0, 1.0, 0.0]);
 
     surface.Draw();
 }
 
-function CreateSurfaceData(maxR = 1)
-{
+function CreateSurfaceData(maxR) {
     let vertexList = [];
+    let normalList = [];
     let step = 0.01;
+    let delta = 0.001;
 
-    for (let r=0.25; r<=maxR; r+=step) {
-        for (let theta=0; theta<=2*Math.PI; theta+=step) {
+    for (let r = 0.25; r <= maxR; r += step) {
+        for (let theta = 0; theta < 2 * Math.PI; theta += step) {
 
-            let x = -(Math.cos(theta) / (2 * r)) - (Math.pow(r, 3) * Math.cos(3 * theta) / 6);
-            let y = -(Math.sin(theta) / (2 * r)) + (Math.pow(r, 3) * Math.sin(3 * theta) / 6);
-            let z = r * Math.cos(theta);
+            let v1 = equations(r, theta);
+            let v2 = equations(r, theta + step);
+            let v3 = equations(r + step, theta);
+            let v4 = equations(r + step, theta + step);
 
-            vertexList.push(x, y, z);
+            vertexList.push(v1.x, v1.y, v1.z);
+            vertexList.push(v2.x, v2.y, v2.z);
+            vertexList.push(v3.x, v3.y, v3.z);
+            
+            vertexList.push(v2.x, v2.y, v2.z);
+            vertexList.push(v4.x, v4.y, v4.z);
+            vertexList.push(v3.x, v3.y, v3.z);
+
+            let n1 = CalculateNormal(r, theta, delta);
+            let n2 = CalculateNormal(r, theta + step, delta);
+            let n3 = CalculateNormal(r + step, theta, delta);
+            let n4 = CalculateNormal(r + step, theta + step, delta)
+
+            normalList.push(n1.x, n1.y, n1.z);
+            normalList.push(n2.x, n2.y, n2.z);
+            normalList.push(n3.x, n3.y, n3.z);
+            
+            normalList.push(n2.x, n2.y, n2.z);
+            normalList.push(n4.x, n4.y, n4.z);
+            normalList.push(n3.x, n3.y, n3.z);
         }
     }
 
-    return vertexList;
+    return { vertices: vertexList, normal: normalList };
+}
+
+function CalculateNormal(r, theta, delta) {
+    let currentPoint = equations(r, theta);
+    let pointR = equations(r + delta, theta);
+    let pointTheta = equations(r, theta + delta);
+
+    let dg_dr = {
+        x: (pointR.x - currentPoint.x) / delta,
+        y: (pointR.y - currentPoint.y) / delta,
+        z: (pointR.z - currentPoint.z) / delta
+    };
+
+    let dg_dtheta = {
+        x: (pointTheta.x - currentPoint.x) / delta,
+        y: (pointTheta.y - currentPoint.y) / delta,
+        z: (pointTheta.z - currentPoint.z) / delta
+    };
+
+    let normal = cross(dg_dr, dg_dtheta);
+
+    normalize(normal);
+
+    return normal;
+}
+
+function equations(r, theta) {
+    let x = -(Math.cos(theta) / (2 * r)) - (Math.pow(r, 3) * Math.cos(3 * theta) / 6);
+    let y = -(Math.sin(theta) / (2 * r)) + (Math.pow(r, 3) * Math.sin(3 * theta) / 6);
+    let z = r * Math.cos(theta);
+
+    return { x: x, y: y, z: z}
+}
+
+function cross(a, b) {
+    let x = a.y * b.z - b.y * a.z;
+    let y = a.z * b.x - b.z * a.x;
+    let z = a.x * b.y - b.x * a.y;
+    return { x: x, y: y, z: z }
+}
+
+function normalize(a) {
+    var b = Math.sqrt(a.x * a.x + a.y * a.y + a.z * a.z);
+    a.x /= b;
+    a.y /= b;
+    a.z /= b;
 }
 
 // Function to update the surface with the new max value of parameter r
 function updateSurface() {
     const maxR = parseFloat(document.getElementById("paramR").value);
-    surface.BufferData(CreateSurfaceData(maxR));
+    let data = CreateSurfaceData(maxR);
+    surface.BufferData(data.vertices, data.normal);
     document.getElementById("currentMaxR").textContent = maxR.toFixed(2);
     draw();
 }
@@ -122,11 +204,14 @@ function initGL() {
     shProgram.Use();
 
     shProgram.iAttribVertex              = gl.getAttribLocation(prog, "vertex");
+    shProgram.iAttribNormal              = gl.getAttribLocation(prog, "normal");
     shProgram.iModelViewProjectionMatrix = gl.getUniformLocation(prog, "ModelViewProjectionMatrix");
-    shProgram.iColor                     = gl.getUniformLocation(prog, "color");
+    shProgram.iModelMatrixNormal         = gl.getUniformLocation(prog, "ModelNormalMatrix");
+    shProgram.iLightPosition             = gl.getUniformLocation(prog, "lightPosition");
 
     surface = new Model('Surface');
-    surface.BufferData(CreateSurfaceData());
+    let data = CreateSurfaceData(1);
+    surface.BufferData(data.vertices, data.normal);
 
     gl.enable(gl.DEPTH_TEST);
 }
